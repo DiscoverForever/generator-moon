aee04153f1e1a6ec5edb7149a4058503a59519ce
tangyy <tangyueyang@lztogether.com>
2018-07-13 13:37:49 +0800

    Fixed one-to-one bug

--- generators/import-jdl/index.js
+++ generators/import-jdl/index.js
@@ -30,28 +30,45 @@ module.exports = class extends Generator {
       const params = entity.javadoc && entity.javadoc.split('?')[1];
       entity.params = params ? querystring.parse(params) : {};
       entity.javadoc = entity.javadoc && entity.javadoc.replace(/\?.*$/g, '');
+      entity.body.forEach(field => {
+        const fieldInjectParams = field.javadoc && field.javadoc.split('?')[1];
+        field.params = fieldInjectParams ? querystring.parse(fieldInjectParams) : {};
+        field.javadoc = field.javadoc && field.javadoc.replace(/\?.*$/g, '');
+      });
     });
     this.jdlObjects.relationships.forEach(relationship => {
       if (relationship.cardinality === 'one-to-one') {
         this.jdlObjects.entities.forEach(entity => {
           if (entity.name === relationship.from.name) {
-            entity.body.push({
-              name: relationship.from.injectedfield
-                ? relationship.from.injectedfield
-                : _.camelCase(relationship.to.name),
-              type: `Pointer_${relationship.to.name}`,
-              validations: [],
-              javadoc: relationship.to.name
-            });
+            // A to B || A {b} to B || A {b} tp B {a}
+            if (
+              (relationship.from.injectedfield === null &&
+                relationship.to.injectedfield === null) ||
+              relationship.from.injectedfield !== null
+            ) {
+              entity.body.push({
+                name: relationship.from.injectedfield
+                  ? relationship.from.injectedfield
+                  : _.camelCase(relationship.to.name),
+                type: `Pointer_${relationship.to.name}`,
+                validations: [],
+                javadoc: relationship.to.javadoc
+                  ? relationship.to.javadoc
+                  : relationship.to.name
+              });
+            }
           } else if (entity.name === relationship.to.name) {
-            entity.body.push({
-              name: relationship.to.injectedfield
-                ? relationship.to.injectedfield
-                : _.camelCase(relationship.from.name),
-              type: `Pointer_${relationship.from.name}`,
-              validations: [],
-              javadoc: relationship.from.name
-            });
+            // A to B {a} || A {b} to B {a}
+            if (relationship.to.injectedfield) {
+              entity.body.push({
+                name: relationship.to.injectedfield,
+                type: `Pointer_${relationship.from.name}`,
+                validations: [],
+                javadoc: relationship.from.javadoc
+                  ? relationship.from.javadoc
+                  : relationship.from.name
+              });
+            }
           }
         });
       } else if (relationship.cardinality === 'one-to-many') {
@@ -63,7 +80,9 @@ module.exports = class extends Generator {
                 : _.camelCase(relationship.from.name),
               type: `Pointer_${relationship.from.name}`,
               validations: [],
-              javadoc: relationship.from.name
+              javadoc: relationship.from.javadoc
+                ? relationship.from.javadoc
+                : relationship.from.name
             });
           }
         });
@@ -76,13 +95,17 @@ module.exports = class extends Generator {
               name: relationship.from.name,
               type: `Pointer_${relationship.from.name}`,
               validations: [],
-              javadoc: relationship.from.name
+              javadoc: relationship.from.javadoc
+                ? relationship.from.javadoc
+                : relationship.from.name
             },
             {
               name: relationship.to.name,
               type: `Pointer_${relationship.to.name}`,
               validations: [],
-              javadoc: relationship.to.name
+              javadoc: relationship.to.javadoc
+                ? relationship.to.javadoc
+                : relationship.to.name
             }
           ],
           javadoc: relationship.from.name + relationship.to.name,
@@ -91,34 +114,23 @@ module.exports = class extends Generator {
       }
     });
     this.config.set('jdlObjects', this.jdlObjects);
-    this.log(JSON.stringify(this.jdlObjects));
   }
 
   async prompting() {}
 
   writing() {
+    this._generateClientProject();
+    this._generateServerProject();
+  }
+
+  install() {}
+
+  _generateServerProject() {
+    if (!this.config.get('serverProjectName')) return;
     this.fs.copyTpl(
       this.templatePath('cloud-import.ts.ejs'),
       this.destinationPath(
         `${this.config.get('serverProjectName')}/src/entities/cloud-import.ts`
-      ),
-      {
-        entities: this.jdlObjects.entities,
-        _
-      }
-    );
-    this.fs.copyTpl(
-      this.templatePath('vue-element-admin/src/lang/'),
-      this.destinationPath(`${this.config.get('clientProjectName')}/src/lang/`),
-      {
-        entities: this.jdlObjects.entities,
-        _
-      }
-    );
-    this.fs.copyTpl(
-      this.templatePath('vue-element-admin/src/router/entities.js'),
-      this.destinationPath(
-        `${this.config.get('clientProjectName')}/src/router/entities.js`
       ),
       {
         entities: this.jdlObjects.entities,
@@ -138,6 +150,53 @@ module.exports = class extends Generator {
           _
         }
       );
+      this.fs.copyTpl(
+        this.templatePath('entity.model.ts.ejs'),
+        this.destinationPath(
+          `${this.config.get('serverProjectName')}/src/entities/${_.kebabCase(
+            entity.name
+          )}/${_.kebabCase(entity.name)}.model.ts`
+        ),
+        {
+          entity,
+          relationships: this.jdlObjects.relationships,
+          enums: this.jdlObjects.enums,
+          dto: this.jdlObjects.dto,
+          _
+        }
+      );
+      this.fs.copyTpl(
+        this.templatePath('role.ts.ejs'),
+        this.destinationPath(`${this.config.get('serverProjectName')}/src/util/role.ts`),
+        {
+          entities: this.jdlObjects.entities,
+          _
+        }
+      );
+    });
+  }
+
+  _generateClientProject() {
+    if (!this.config.get('clientProjectName')) return;
+    this.fs.copyTpl(
+      this.templatePath('vue-element-admin/src/lang/'),
+      this.destinationPath(`${this.config.get('clientProjectName')}/src/lang/`),
+      {
+        entities: this.jdlObjects.entities,
+        _
+      }
+    );
+    this.fs.copyTpl(
+      this.templatePath('vue-element-admin/src/router/entities.js'),
+      this.destinationPath(
+        `${this.config.get('clientProjectName')}/src/router/entities.js`
+      ),
+      {
+        entities: this.jdlObjects.entities,
+        _
+      }
+    );
+    this.jdlObjects.entities.forEach(entity => {
       this.fs.copyTpl(
         this.templatePath('vue-element-admin/src/views/entity/index.vue'),
         this.destinationPath(
@@ -188,31 +247,6 @@ module.exports = class extends Generator {
           _
         }
       );
-      this.fs.copyTpl(
-        this.templatePath('entity.model.ts.ejs'),
-        this.destinationPath(
-          `${this.config.get('serverProjectName')}/src/entities/${_.kebabCase(
-            entity.name
-          )}/${_.kebabCase(entity.name)}.model.ts`
-        ),
-        {
-          entity,
-          relationships: this.jdlObjects.relationships,
-          enums: this.jdlObjects.enums,
-          dto: this.jdlObjects.dto,
-          _
-        }
-      );
-      this.fs.copyTpl(
-        this.templatePath('role.ts.ejs'),
-        this.destinationPath(`${this.config.get('serverProjectName')}/src/util/role.ts`),
-        {
-          entities: this.jdlObjects.entities,
-          _
-        }
-      );
     });
   }
-
-  install() {}
 };
